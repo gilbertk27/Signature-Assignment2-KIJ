731cbb30a694728b59bd06a8dbadb6988a020c5d
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2022-10-30 11:24:24 +0700

    add keygen

--- keygen.py
+++ keygen.py
@@ -4,9 +4,13 @@ from contextlib import redirect_stdout
 
 print("Generating Public & Private Key")
 key = RSA.generate(2048)
-p_key = key.publickey().exportKey(RSA)
-priv_key = key.exportKey(RSA)
+p_key = key.publickey().exportKey("PEM")
+priv_key = key.exportKey("PEM")
 
-with open('out.txt', 'w') as f:
+with open('pubkey.out', 'w') as f:
     with redirect_stdout(f):
-        print('p_key')
+        print(p_key)
+        
+with open('privkey.out', 'w') as f:
+    with redirect_stdout(f):
+        print(priv_key)
